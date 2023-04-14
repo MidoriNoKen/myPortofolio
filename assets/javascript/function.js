009d2a28b8b57b807b8913d48e21814fb5b36eb3
MidoriNoKen <midor1nok3n@gmail.com>
2023-04-14 23:58:59 +0700

    Add My Field Section

---
+++ assets/javascript/function.js
@@ -0,0 +1,12 @@
+// this is JQuery but there is a react equivalent - https://react-slick.neostack.com/  
+$(document).on('ready', function() {
+    $('.slider').slick({
+      dots: true,
+      centerMode: true,
+      centerPadding: '1em',
+      infinite: true,
+      lazyLoad: 'ondemand',
+      slidesToShow: 3,
+      slidesToScroll: 3,
+    });
+  });
